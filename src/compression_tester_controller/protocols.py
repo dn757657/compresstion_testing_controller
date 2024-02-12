20fa848c16ce5a9266b0968fe43136a15d98cef6
Daniel Campbell <daniel.js.campbell@gmail.com>
2024-02-12 15:57:23 -0400

    why no work

--- src/compression_tester_controller/protocols.py
+++ src/compression_tester_controller/protocols.py
@@ -1,73 +1,78 @@
 import logging
-import threading
-import time 
 
 logging.basicConfig()
 logging.getLogger().setLevel(logging.INFO)
 
-# from sqlalchemy import select
+from sqlalchemy import select
 
-# from compression_testing_data.main import parse_gphoto_config_for_sql, parse_cam_config_dict_for_gphoto, parse_sql_gphoto_config_for_gphoto
-# from compression_testing_data.meta import Session
-# from compression_testing_data.models.samples import Print, Sample
-# from compression_testing_data.models.acquisition_settings import CameraSetting
-# from compression_testing_data.models.testing import CompressionTrial, CompressionStep
+from compression_testing_data.main import parse_gphoto_config_for_sql, parse_cam_config_dict_for_gphoto, parse_sql_gphoto_config_for_gphoto
+from compression_testing_data.meta import Session
+from compression_testing_data.models.samples import Print, Sample
+from compression_testing_data.models.acquisition_settings import CameraSetting
+from compression_testing_data.models.testing import CompressionTrial, CompressionStep
 
 from compression_tester_controls.components.canon_eosr50 import gphoto2_get_active_ports, gpohoto2_get_camera_settings, eosr50_continuous_capture_and_save    
 from compression_tester_controls.sys_protocols import platon_setup, init_cameras, sys_init, home_camera_system, capture_step_frames, camera_system_setup
 from compression_tester_controls.sys_functions import sample_force_sensor
 
 
-# def store_camera_settings(port = None):
-#     if not port:
-#         ports = gphoto2_get_active_ports()
-#         port = ports[0]
+def store_camera_settings(port = None):
+    if not port:
+        ports = gphoto2_get_active_ports()
+        port = ports[0]
 
-#     # get camera settings table columns
-#     cam_settings_sql = [column.key for column in CameraSetting.__table__.columns]
-#     sql_settings_exclude = ['id', 'created_at']  # filter out non camera settings
-#     cam_settings_sql = [x for x in cam_settings_sql if x not in sql_settings_exclude]
+    # get camera settings table columns
+    cam_settings_sql = [column.key for column in CameraSetting.__table__.columns]
+    sql_settings_exclude = ['id', 'created_at']  # filter out non camera settings
+    cam_settings_sql = [x for x in cam_settings_sql if x not in sql_settings_exclude]
 
-#     config = gpohoto2_get_camera_settings(port=port, config_options=cam_settings_sql)
-#     logging.info(f"Retrieving Settings: {cam_settings_sql} from Camera @ {port}")
+    config = gpohoto2_get_camera_settings(port=port, config_options=cam_settings_sql)
+    logging.info(f"Retrieving Settings: {cam_settings_sql} from Camera @ {port}")
 
-#     dict_config = parse_gphoto_config_for_sql(config_output=config)
-#     logging.info(f"Storing: {dict_config}")
+    dict_config = parse_gphoto_config_for_sql(config_output=config)
+    logging.info(f"Storing: {dict_config}")
 
-#     session = Session()
-#     session.add(CameraSetting(**dict_config))
-#     session.commit()
-#     session.close()
-#     logging.info(f"Stored Current Camera Settings.")
+    session = Session()
+    session.add(CameraSetting(**dict_config))
+    session.commit()
+    session.close()
+    logging.info(f"Stored Current Camera Settings.")
 
-#     pass
+    pass
 
 
-# def get_cam_settings(id: int = 1):
-#     session = Session()
+def get_cam_settings(id: int = 1):
+    session = Session()
 
-#     stmt = select(CameraSetting).where(CameraSetting.id == id)
-#     slct = session.execute(stmt)
+    stmt = select(CameraSetting).where(CameraSetting.id == id)
+    slct = session.execute(stmt)
 
-#     settings = slct.scalars().all()
-#     if len(settings) > 1:
-#         logging.info("found too many camera settings?")
-#         return
-#     else:
-#         setting = settings[0]
+    settings = slct.scalars().all()
+    if len(settings) > 1:
+        logging.info("found too many camera settings?")
+        return
+    else:
+        setting = settings[0]
 
-#     setting = parse_sql_gphoto_config_for_gphoto(camera_setting=setting)
+    setting = parse_sql_gphoto_config_for_gphoto(camera_setting=setting)
 
-#     print(f"gphoto settings: {setting}")
+    print(f"gphoto settings: {setting}")
 
-#     return setting
+    return setting
 
 def run_trial():
     components = sys_init()
     encoder_zero_count, encoder_sample_height_count = platon_setup(components=components)
-    # get sample height in mm - log to db
+    sample_height = abs(encoder_sample_height_count - encoder_zero_count)
+    # translate to mm
+    # push to db
 
     camera_system_setup(components=components)
+
+    # create steps - ensure first step is zero - limit to 2mm max compression
+    # run steps one by one
+    # move platon
+    # check for force overload
 
     # run_trial_step(components=components)
     return

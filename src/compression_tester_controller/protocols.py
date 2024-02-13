92c24b1fd911764bcf4cb989e9161927a7984645
Daniel Campbell <daniel.js.campbell@gmail.com>
2024-02-13 10:47:24 -0400

    testing force sensor rf

--- src/compression_tester_controller/protocols.py
+++ src/compression_tester_controller/protocols.py
@@ -4,6 +4,7 @@ logging.basicConfig()
 logging.getLogger().setLevel(logging.INFO)
 
 from sqlalchemy import select
+import numpy as np
 
 from compression_testing_data.main import parse_gphoto_config_for_sql, parse_cam_config_dict_for_gphoto, parse_sql_gphoto_config_for_gphoto
 from compression_testing_data.meta import Session
@@ -60,59 +61,82 @@ def get_cam_settings(id: int = 1):
 
     return setting
 
-def run_trial():
+
+def encoder_to_mm(encoder_steps):
+    mm = encoder_steps * (6/1000)
+    return mm
+
+
+def find_force_sensor_Rf():
+    components = sys_init()
+    a201 = components.get("A201")
+
+    rf = a201.get_rf(rs=1000)
+    print(f"{rf}")
+
+    return
+
+def run_trial(trial_id: int = 11):
     components = sys_init()
     encoder_zero_count, encoder_sample_height_count = platon_setup(components=components)
-    sample_height = abs(encoder_sample_height_count - encoder_zero_count)
+    sample_height_counts = abs(encoder_sample_height_count - encoder_zero_count)
+    print(f"Sample Height: {encoder_to_mm(encoder_sample_height_count)}")
+    # pull sample from DB! can full from id in trial
+    # pull trial from db
     # translate to mm
     # push to db
 
+    force_zero = np.mean(sample_force_sensor(n_samples=100, components=components))
+    print(f"Force @ Step: {force_zero}")
+    # push force zero to trial in db
+
     camera_system_setup(components=components)
+    # from trial we get
+    # force limit
+    # strain limit
+    # strain delta
+    strain_min = 0
+    desired_strain_limit = 0.8
+    desired_strain_delta = 0.1
+    force_limit = 1000
 
-    # create steps - ensure first step is zero - limit to 2mm max compression
-    # run steps one by one
-    # move platon
-    # check for force overload
+    step_strain = strain_min
+    while True:
 
-    # run_trial_step(components=components)
+        print(i)  # do the step using i as stran
+        step_strain += desired_strain_delta
+
+        if step_strain > desired_strain_limit:
+            logging.info("Strain limit reached! Trial Complete")
+            break
+
+        # create steps - ensure first step is zero - limit to 2mm max compression
+        # run steps one by one
+        # move platon
+        # check for force overload
+        # get photos
+
+        # run_trial_step(components=components)
     return
 
-# def run_trial_step(components):
-#     # ensure dir structure for frames? - do this in the transfer handler
-#     # move crusher to strain desired
+def run_trial_step(components):
+    # ensure dir structure for frames? - do this in the transfer handler
+    # move crusher to strain desired
 
-#     # force = np.mean(sample_force_sensor(n_samples=100, components=components))
-#     # print(f"Force @ Step: {force}")
+    force = np.mean(sample_force_sensor(n_samples=100, components=components))
+    print(f"Force @ Step: {force}")
 
-#     cam_settings = get_cam_settings(id=1)  # get cam settings from steps object!
-#     cam_ports = init_cameras(cam_settings=cam_settings)
-
-#     # maybe add the base dir these are going to also
-#     for i in range(0, 2):
-#         force = sample_force_sensor(n_samples=100, components=components)
-#         print(f"Force @ Step: {force}")
+    cam_settings = get_cam_settings(id=1)  # get cam settings from steps object!
+    cam_ports = init_cameras(cam_settings=cam_settings)
         
-#         photo_list = capture_step_frames(cam_ports=cam_ports, components=components, stepper_freq=500)
-#         print(f"step Photos: {photo_list}")
-#     # take photo, push to db
+    photo_list = capture_step_frames(cam_ports=cam_ports, components=components, stepper_freq=500)
+    print(f"step Photos: {photo_list}")
+    # move photos to server
+    # enter photos in server db
 
-#     # TODO need to complete sample, this can also be done post, since it involves volume:
-#     #   - essentially if a strain step target is 0 then we can derive the sample height and diam
-#     #   - add height after first scan?? or should this be in the steps or stl?
-
-#     # get force zero - get n samples from adc channel obs and avg before moving motor
-#     #   store in compression trial
-#     # find encoder zero - do we store this somewhere
-#     #   store on machine - add method to encoder class to reset encoder count at lower platon
-#     # make steps
-#     # run steps, run in order of descending strain
-#     #   move motor
-#     #       first step use special protocol to line up to sample
-#     #   complete step, record strain, force, etc
-#     #   transfer frames and make entries
-#     #   check entries using base dir in dir structure
-#     return
+    return
 
 
 if __name__ == '__main__':
-    run_trial()
+    # run_trial()
+    find_force_sensor_Rf()
